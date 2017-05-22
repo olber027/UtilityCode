d943ba0af1c11c99e77cc00a7557f78bdda1ac3c
olber027 <olber027@gmail.com>
2017-05-22 09:50:07 -0500

    Initial commit

---
+++ AStar.cpp
@@ -0,0 +1,7 @@
+#include "AStar.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ AStar.h
@@ -0,0 +1,8 @@
+#ifndef UTILITYCODE_LIBRARY_H
+#define UTILITYCODE_LIBRARY_H
+
+class Node {
+
+};
+
+#endif
