54d16288b01ed354c3c6d21c4951bc3c9a04a91d
olber027 <olber027@gmail.com>
2017-07-19 15:09:14 -0500

    adding the node for the binary tree and the binary tree constructors

--- DataStructures/BinaryTree.h
+++ DataStructures/BinaryTree.h
@@ -16,9 +16,56 @@ namespace binary_tree {
     struct is_less_than_comparable<T, typename std::enable_if<std::is_convertible<decltype(std::declval<T&>() < std::declval<T&>()), bool>{}>::type> : std::true_type{};
 
     template<typename T>
+    class Node{
+    private:
+        Node<T>* right;
+        Node<T>* left;
+        T data;
+    public:
+        Node(T d) : right(nullptr), left(nullptr), data(d) {}
+
+        Node(T d, Node<T>* r, Node<T>* l) : right(r), left(l), data(d) {}
+
+        Node(const Node<T> &node) {
+            if(this != &node) {
+                data = node.data;
+                left = node.left;
+                right = node.right;
+            }
+        }
+
+        Node<T>& operator=(const Node<T> &rhs) {
+            if(this != &rhs) {
+                data = rhs.data;
+                left = rhs.left;
+                right = rhs.right;
+            }
+            return *this;
+        }
+
+        bool operator<(Node<T> other) {
+            return data < other.data;
+        }
+
+        bool operator>(Node<T> other) {
+            return other.data < data;
+        }
+
+        T getdata() {
+            return data;
+        }
+    };
+
+    template<typename T>
     class BinaryTree{
         static_assert(is_less_than_comparable<T>::value, "Type T must be comparable with the < operator");
 
+    private:
+        Node<T>* root;
+
+    public:
+        BinaryTree() : root(nullptr) {}
+        BinaryTree(Node<T>* r) : root(r) {}
 
     };
 }
