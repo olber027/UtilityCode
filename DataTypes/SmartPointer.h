024a6cf15133e16a83366a58655eb0192ba0bb74
olber027 <olber027@gmail.com>
2017-09-13 10:40:30 -0500

    finished Smart Pointer implementation, and added corresponding testing.

--- DataTypes/SmartPointer.h
+++ DataTypes/SmartPointer.h
@@ -5,24 +5,69 @@
 #ifndef UTILITYCODE_SMARTPOINTER_H
 #define UTILITYCODE_SMARTPOINTER_H
 
+#include <iostream>
+
 namespace smart_pointer {
 
     template<typename T>
     class SmartPointer {
     private:
-        T* pointer;
-        int numReferences;
-    public:
-        SmartPointer() : pointer(nullptr), numReferences(0) { }
-        SmartPointer(T* val) : pointer(val), numReferences(1) { }
-        SmartPointer(T val) : pointer(new T(val)), numReferences(1) { }
 
-        T& operator*() {
-            return *pointer;
+        class ReferenceCounter {
+        private:
+            int numReferences;
+        public:
+            ReferenceCounter() : numReferences(0) {}
+            ReferenceCounter(int init) : numReferences(init) {}
+            int addRef() { return ++numReferences; }
+            int removeRef() { return --numReferences; }
+            int get() { return numReferences; }
+        };
+
+        T* pointer;
+        ReferenceCounter* referenceCounter;
+
+    public:
+        SmartPointer() : pointer(nullptr) {
+            referenceCounter = new ReferenceCounter();
         }
 
-        T* operator->() {
-            return pointer;
+        SmartPointer(T* val) : pointer(val) {
+            referenceCounter = new ReferenceCounter(1);
+        }
+
+        SmartPointer(const SmartPointer<T>& smartPointer) {
+            pointer = smartPointer.pointer;
+            referenceCounter = smartPointer.referenceCounter;
+            referenceCounter->addRef();
+        }
+
+        void destroy() {
+            if(referenceCounter->removeRef() == 0) {
+                delete referenceCounter;
+                delete pointer;
+                pointer = nullptr;
+            }
+        }
+
+        ~SmartPointer() {
+            destroy();
+        }
+
+        T& operator*() { return *pointer; }
+        T* operator->() { return pointer; }
+        T* getPointer() { return pointer; }
+        int numReferences() { return referenceCounter->get(); }
+
+        SmartPointer<T>& operator=(const SmartPointer<T>& rhs) {
+            if(&rhs != this) {
+                destroy();
+
+                pointer = rhs.pointer;
+                referenceCounter = rhs.referenceCounter;
+                referenceCounter->addRef();
+            }
+            return *this;
         }
     };
 }
--- Testing/SmartPointerTestDriver.h
+++ Testing/SmartPointerTestDriver.h
@@ -36,6 +36,46 @@ public:
     void run() {
         assert(5, *intPointer);
         assert(7, testObjectPointer->get());
+        for(int i = 0; i < 5; i++) {
+            SmartPointer<TestObject> temp = testObjectPointer;
+            assert(7, temp->get());
+        }
+        assert(7, testObjectPointer->get());
+        SmartPointer<TestObject> newPointer = SmartPointer<TestObject>(new TestObject(11));
+        assert(11, newPointer->get());
+        testObjectPointer = newPointer;
+        assert(11, testObjectPointer->get());
+        SmartPointer<TestObject> pointer1 = testObjectPointer;
+        assert(11, pointer1->get());
+        SmartPointer<TestObject> pointer2 = testObjectPointer;
+        assert(11, pointer2->get());
+        SmartPointer<TestObject> pointer3 = testObjectPointer;
+        assert(11, pointer3->get());
+        SmartPointer<TestObject> pointer4 = testObjectPointer;
+        assert(11, pointer4->get());
+        newPointer.destroy();
+        assert(11, pointer1->get());
+        assert(11, pointer2->get());
+        assert(11, pointer3->get());
+        assert(11, pointer4->get());
+        testObjectPointer.destroy();
+        assert(11, pointer1->get());
+        assert(11, pointer2->get());
+        assert(11, pointer3->get());
+        assert(11, pointer4->get());
+        pointer1.destroy();
+        assert(11, pointer2->get());
+        assert(11, pointer3->get());
+        assert(11, pointer4->get());
+        pointer2.destroy();
+        assert(11, pointer3->get());
+        assert(11, pointer4->get());
+        pointer3.destroy();
+        assert(11, pointer4->get());
+        TestObject* dumbPointer = pointer4.getPointer();
+        assert(11, dumbPointer->get());
+        assert(1, pointer4.numReferences());
+        pointer4.destroy();
     }
 };
 
